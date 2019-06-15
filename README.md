094205db72b392b274d076b42ba58c25fd7b9331
Daniel Sagi <danielsagi2009@gmail.com>
2019-06-16 00:25:16 +0300

    Added auto_download script. also documented in the README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # magshimim-teacher-tools
 A collection of tools for Magshimim teachers
+
+## Tools:
+### auto_download.py
+This tool is for downloading students submitted files automatically,  
+
+#### Steps to configure:
+1. change the raw_cookies variable to your cookie.
+2. acquire your class id:  
+    this is achieved by clicking on your class in EDU,  
+    looking at url:
+    https://magshimim.edu20.org/teacher_lessons/list/<class_id>
+3. acquire your task id:  s
+    this is achieved by clicking on your desired assignment in the assignments tabs in EDU,  
+    looking at url:
+    https://magshimim.edu20.org/teacher_freeform_assignment/show/<task_id>
+4. run the script with the ids as parameters.
+
+The script will scrape all of your students files automatically, and for each student, create a folder named with his name, containing his submitted files.
+
+The folders will be created next to the script's executed path
---
+++ auto_download.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -------------------------
+# Written by: Daniel Sagi
+# -------------------------
+import os
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+s = requests.Session()
+
+raw_cookies = "" # change to content of Cookies from http requests
+s.headers.update({"Cookie": raw_cookies})
+
+if len(sys.argv) < 3:
+    print("Usage: ./auto_download.py <class_id> <task_id>")
+    sys.exit(1)
+
+BASE_URL = "https://magshimim.edu20.org"
+CLASS_ID = sys.argv[1] # example: "1138084"
+TASK_ID = sys.argv[2] # example: "8796494"
+
+
+def download_students_files(students, task_id):
+    for student_id, student_name in students.items():
+        print("[+] Downloading files for {}".format(student_name))
+        full_url = BASE_URL + "/teacher_freeform_assignment/grade/{}?offset=0&student={}".format(task_id, student_id)
+        soup = BeautifulSoup(s.get(full_url).text, features="html.parser")
+
+        for a in soup.find_all('a', href=True):
+            if "/files/" in a['href']:
+                f_name = '/' + a['href'].split('/')[3]
+                with open(student_name + f_name, 'wb') as f: 
+                    f.write(s.get(BASE_URL + a['href']).content)
+
+
+def get_student_list(class_id):
+    full_url = BASE_URL + "/teacher_students/list/{}".format(class_id)
+    soup = BeautifulSoup(s.get(full_url).text, features="html.parser")
+    
+    students = dict()
+    for a in soup.find_all('a', href=True):
+        if '/user/show/' in a['href'] and not a.find('img'):
+            student_name = a.text.replace(', ', '_')
+            student_id = a['href'].split('/')[3]
+            students[student_id] = student_name
+    return students
+
+
+def create_student_folders(student_names):
+    for name in student_names:
+        try:
+            os.mkdir(name)
+        except FileExistsError:
+            pass
+
+
+def main():
+    students = get_student_list(CLASS_ID)
+    create_student_folders(students.values())
+
+    # downloading files of students to their corresponding folder
+    download_students_files(students, TASK_ID)
+
+if __name__ == '__main__':
+    main()
